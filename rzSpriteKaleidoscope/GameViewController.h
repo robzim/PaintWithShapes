a9fa1f98536ac252c093f98a5d30e92bd0845306
robzim <Rob Zimmelman>
2015-12-11 15:14:33 -0500

    Initial Commit

---
+++ rzSpriteKaleidoscope/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  rzSpriteKaleidoscope
+//
+
+//  Copyright (c) 2015 Robert Zimmelman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
